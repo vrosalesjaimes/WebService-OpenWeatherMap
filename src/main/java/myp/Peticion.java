6f7a2fc7af0de536bbe232f036f50cbd44fac9df
Victor Rosales <vrosalesjaimes@ciencias.unam.mx>
2022-03-10 19:46:30 -0600

    Agregando actualización para clima

--- src/main/java/myp/Peticion.java
+++ src/main/java/myp/Peticion.java
@@ -6,8 +6,6 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.http.*;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Esta clase se encargará de crear un url para
@@ -21,24 +19,33 @@ public class Peticion{
     /*Código iata de la ciudad*/
     private String codeIata;
 
-    //Constructor
+    /**
+     * Constructor de la clase Peticion.
+     * @param codeIata, código iata de la ciudad para saber su estado de tiempo.
+     * @param longitud, longitud de la ciudad para saber su estado de tiempo.
+     * @param latitud, latitud de la ciudad para saber su estado de tiempo
+     */
     public Peticion(String codeIata, String latitud, String longitud){
 	this.codeIata  = codeIata;
 	this.latitud   = latitud;
 	this.longitud  = longitud;
     }
 
+    /**
+     * Regresa el codigo iata de una petición.
+     * @return codeIata, el código de la ciudad.
+     */
     public String getCodeIata(){
 	return codeIata;
     }
 
     /**
-     * Hacemos la petición y la guardamos en un StringBuffer, en el video que vi
-     * hacía con StringBuilder, pero ví que con StringBuffer es que tiene su métodos
-     * sincronizados y podemos trabajar con varios hilos.
+     * Hace una llamada al servidor de OpenWatherMap.
+     * @param peticion, la peticion con la que llamaremos al servidor.
+     * @return la representación en cadena de la respuesta del servidor.
      */
-    private static String llamaServidor(Peticion p){
-	String urlString = "https://pro.openweathermap.org/data/2.5/onecall?lat=" + p.latitud + "&lon=" + p.longitud+ "&units=metric&exclude=hourly,daily,minutely&lang=sp&appid=5aac2dc7618e3fd43f31263400bc1788";
+    private static String llamaServidor(Peticion peticion){
+	String urlString = "https://pro.openweathermap.org/data/2.5/onecall?lat=" + peticion.latitud + "&lon=" + peticion.longitud+ "&units=metric&exclude=hourly,daily,minutely&lang=sp&appid=5aac2dc7618e3fd43f31263400bc1788";
 	String respuesta = new String();
 	try{
 	    URL url =  new URL(urlString);
@@ -52,12 +59,16 @@ public class Peticion{
 	return respuesta;
     }
 
-    public static String daFormatoCsv(Peticion p){
-	String s = llamaServidor(p);
+    /**
+     * Da un formato a la respuesta del servidor con el fin de ser escrito en un archivo csv.
+     * @param peticion, la peticion de la cual queremos convertir a una linea con comas.
+     * @return representacion en cadena separada por comas de la respuesta del servidor.
+     */
+    public static String daFormatoCsv(Peticion peticion){
+	String s = llamaServidor(peticion);
 	String[] respuesta = s.split(",");
 	String temp = respuesta[7].replace("\"temp\":","") + "°C";
 	String sensacion = respuesta[8].replace("\"feels_like\":","")+"°C";
-	String descripcion =  respuesta[20].replaceAll("\"description\":","").replace("\"","");
-	return temp+ ", " + sensacion + ", " + descripcion + ",";
+	return temp+ ", " + sensacion + ",";
     }
 }
--- src/main/java/myp/Proyecto01.java
+++ src/main/java/myp/Proyecto01.java
@@ -1,9 +1,6 @@
 package myp;
 
-import java.util.concurrent.ConcurrentHashMap;
-import java.io.File;
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.FileInputStream;
@@ -13,16 +10,21 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 
 public class Proyecto01{
-
+    /*Cache de peticiones*/
     static HashMap<String, String> cache = new HashMap<String, String>();
 
+    /**
+     * Lee un archivo de tipo csv, crea dos objectos de tipo Peticion
+     * para imprimir una linea con el clima de la ciudad origen y ciudad destino.
+     * @param archivo, el nombre del archivo.
+     */
     private static void procesaCsv(String archivo){
 	try{
 	    BufferedReader csv = new BufferedReader(new InputStreamReader(new FileInputStream(archivo)));
 	    BufferedWriter out = new BufferedWriter(
 				     new OutputStreamWriter(
 					 new FileOutputStream("salida.csv")));
-	    out.write("Origen, Temperatura, Sensación térmica, Descripción, Destino, Temperatura, Sensación términa, Descripción");
+	    out.write("Origen, Temperatura, Sensación térmica, Destino, Temperatura, Sensación términa" + "\n");
 	    String linea = csv.readLine();
 	    while((linea = csv.readLine()) != null){
 		String[] viaje = linea.split(",");
@@ -35,6 +37,13 @@ public class Proyecto01{
 	}
     }
 
+    /**
+     * Revisa si ya se ha creado un objecto de tipo Peticiony se ha añadido al cache, 
+     * en caso de que no lo añade.
+     * @param peticion, la peticion a buscar o añadir al cache.
+     * @return representación en cadena separado por comas del estado de
+     * tiempo en lasss ciudades de origen y destino.
+     */
     private static String esUnaPeticionRepetida(Peticion p){
 	if(cache.containsKey(p.getCodeIata())){
 	    return cache.get(p.getCodeIata());
@@ -47,7 +56,21 @@ public class Proyecto01{
 
 
     public static void main(String[] args) {
-	String nombreCsv = "dataset1.csv";
-	procesaCsv(nombreCsv);
+	Runnable runnable = new Runnable() {
+		@Override public void run() {
+		    while (true) {
+			try {
+			    Thread.sleep(360000);
+			    procesaCsv("dataset1.csv");
+			} catch (InterruptedException e) {
+			    System.out.println("Algo inesperado ha ocurrido");
+			    System.exit(1);
+			}
+		    }
+		}
+	    };
+	Thread hilo = new Thread(runnable);
+	hilo.start();
+	procesaCsv("dataset1.csv");
     }
 }
