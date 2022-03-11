eefc40474ca71ebe097cd45dcfb01ed35498c87a
José Angel Valerio <joseavalerio@ciencias.unam.mx>
2022-03-10 20:26:23 -0600

    Añadido caso donde la latitud y longitud son incorrectas. Añadida prueba unitaria para representar esto. Editado modificador de acceso de llamaServidor.

--- src/main/java/myp/Peticion.java
+++ src/main/java/myp/Peticion.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.http.*;
+import java.text.NumberFormat;
 
 /**
  * Esta clase se encargará de crear un url para
@@ -44,7 +45,9 @@ public class Peticion{
      * @param peticion, la peticion con la que llamaremos al servidor.
      * @return la representación en cadena de la respuesta del servidor.
      */
-    private static String llamaServidor(Peticion peticion){
+    public static String llamaServidor(Peticion peticion){
+	if (!esValido(peticion))
+		return "Error"; //Pendiente
 	String urlString = "https://pro.openweathermap.org/data/2.5/onecall?lat=" + peticion.latitud + "&lon=" + peticion.longitud+ "&units=metric&exclude=hourly,daily,minutely&lang=sp&appid=5aac2dc7618e3fd43f31263400bc1788";
 	String respuesta = new String();
 	try{
@@ -66,9 +69,33 @@ public class Peticion{
      */
     public static String daFormatoCsv(Peticion peticion){
 	String s = llamaServidor(peticion);
+	if (s.equals("Error"))
+		return "Error"; //Pendiente
 	String[] respuesta = s.split(",");
 	String temp = respuesta[7].replace("\"temp\":","") + "°C";
 	String sensacion = respuesta[8].replace("\"feels_like\":","")+"°C";
 	return temp+ ", " + sensacion + ",";
     }
+
+	/**
+	 * Analiza una instancia de {@link Peticion} y devuelve {@code true} si su
+	 * latitud y longitud es válida, {@code false} en otro caso.
+	 * @param p la petición a analizar,
+	 * @return {@code true} si la longitud y latitud de la petición es válida,
+	 * 		   {@code false} en caso contrario.
+	 */
+	private static boolean esValido(Peticion p){
+		double latitud, longitud;
+		try{
+			latitud = Double.parseDouble(p.latitud);
+			longitud = Double.parseDouble(p.longitud);
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+		if (latitud < -90 || latitud > 90)
+			return false;
+		if (longitud < -180 || longitud > 180)
+			return false;
+		return true;
+	}
 }
