75d0171ead24daeabb764323b794b5361aa67661
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2023-01-06 15:14:41 -0600

    Me di cuenta de que era mejor para la clase Cache extender un diccionario, así no es necesirio escribir más metodos

--- src/main/java/myp/Cache.java
+++ src/main/java/myp/Cache.java
@@ -5,64 +5,11 @@ import java.util.HashMap;
 /**
  * Clase que representa un Caché.
  */
-public class Cache {
-	/* Colección del cache */
-	private HashMap<String, String> cache;
-
+public class Cache extends HashMap<String,String> {
 	/**
 	 * Constructor
 	 */
 	public Cache() {
-		this.cache = new HashMap<String, String>();
+		super();
 	}
-
-	/**
-	 * Regresa el diccionario que contiene al caché.
-	 * 
-	 * @return el diccionario que contiene al caché.
-	 */
-	public HashMap<String, String> getCache() {
-		return cache;
-	}
-
-	/**
-	 * Cambia el diccionario que contiene al caché.
-	 * 
-	 * @param cache, el nuevo caché.
-	 */
-	public void setCache(HashMap<String, String> cache) {
-		this.cache = cache;
-	}
-
-	/**
-	 * Regresa el elemento identificado con la llave key.
-	 * 
-	 * @param key, la llave del valor buscado.
-	 * @return el valor de la llave key.
-	 */
-	public String get(String key) {
-		return this.cache.get(key);
-	}
-
-	/**
-	 * Agrega un elemento al caché.
-	 * 
-	 * @param key,   la llave del nuevo elemento en el caché.
-	 * @param value, el valor del nuevo elemento en el caché.
-	 */
-	public void put(String key, String value) {
-		this.cache.put(key, value);
-	}
-
-	/**
-	 * Regresa true si exise un elemento con la llave proporcionada,
-	 * false en otro caso.
-	 * 
-	 * @param key, la llave que se busca en el cache.
-	 * @return el elemento de la llave key.
-	 */
-	public boolean containsKey(String key) {
-		return this.cache.containsKey(key);
-	}
-
 }
